a6fda364c35283c662ae89afabe41a7d3c8373b5
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-09-26 06:25:16 +0000

    [Finish]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
-  
+  resources :post_images, only: [:new, :create, :index, :show]
+
   get "/homes/about" => "homes#about", as: "about"
 end
